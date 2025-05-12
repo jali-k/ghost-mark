9d557b3703deb1be7f4ad2727880e5adb5768425
jali-k <e19210@eng.pdn.ac.lk>
2025-05-12 18:07:28 +0530

    Fix extracting watermark

--- ghost_mark/pdf_app/models.py
+++ ghost_mark/pdf_app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+
+class WatermarkedDocument(models.Model):
+    document = models.FileField(upload_to="documents/")
+    watermarked_document = models.FileField(
+        upload_to="watermarked_documents/", null=True, blank=True
+    )
+    watermark_text = models.CharField(max_length=255)
+    watermark_color = models.CharField(max_length=7, default="#fffffe")
+    upload_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Document {self.id} - {self.watermark_text}"
